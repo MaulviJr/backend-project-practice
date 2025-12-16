e1289605f09871d0b8fc3e409c4131c65b4d3a86
MaulviJr <abdulhadighouri123@gmail.com>
2025-12-16 09:03:02 +0500

    multer, cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+export default cloudinary;
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+      const response =  cloudinary.uploader.upload(localFile,{
+    resource_type:"auto"
+})
+    console.log("File Uploaded");
+    console.log("response material check: ", response)
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        // remove the temp file in public
+    }
+}
+
