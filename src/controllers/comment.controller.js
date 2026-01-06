b18295fdb2d8f3614ff766481e4986e6e95efa92
MaulviJr <abdulhadighouri123@gmail.com>
2026-01-06 16:00:05 +0500

    feat: implement comment functionality with CRUD operations; add get, add, update, and delete comment methods

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,164 @@
+import mongoose from "mongoose"
+import {Comment} from "../models/comment.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+    const commentQuery = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },{
+            $lookup: {
+                from: "users",
+                localField:"owner",
+                foriegnField:"_id",
+                as: "owner",
+                pipeline:[
+                    {
+                        $project:{
+                            username: 1,
+                            avatar:1,
+                            fullName:1,
+                        }
+                   },
+                  
+                ]
+            }
+        },
+        {
+            $addfields:{
+                $first:"$owner"
+            }
+        },
+        {
+            $sort: { createdAt: -1 }
+        }
+
+    ])
+   
+
+
+     const options = {
+        page: parseInt(page,10),
+        limit: parseInt(limit,10)
+    }
+
+    const result = await Comment.aggregatePaginate(commentQuery,options);
+
+    if(!result) {
+        throw new ApiError(400,"Couldn't find comments")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            result,
+            "Comments fetched successfully"
+        )
+    )
+})
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+    const {content} = req.body;
+    const {videoId} = req.params;
+
+if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video ID format");
+    }
+
+    if(!content) {
+        throw new ApiError(400,"Content is Required");
+    }
+
+    const newComment =await Comment.create({
+        content: content,
+        video: videoId,
+        owner: req.user?._id,
+    })
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            newComment,
+            "Comments was added"
+        )
+    )
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+    const {commentId} = req.params;
+    const {content} = req.body; 
+    if(!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400,"The commentId is Invalid")
+    }
+
+   const updateComment = await Comment.findOne({
+    _id:commentId
+   })
+
+   if(!updateComment) {
+        throw new ApiError(400,"Couldn't fetch Comment");
+   }
+
+   updateComment.content = content;
+
+   await updateComment.save()
+
+     return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updateComment,
+            "Comments was updated"
+        )
+    )
+
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+       const {commentId} = req.params;
+
+         if(!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400,"The commentId is Invalid")
+    }
+       const comment = await Comment.findById(commentId);
+
+       if(!comment) {
+        throw new ApiError(400,"The comment couldn't be fetched")
+       }
+
+       if(comment.owner.toString()!==req.user?._id.toString()) {
+        throw new ApiError(400,"The user is not the owner of the Comment")
+       }
+        
+
+       await Comment.findByIdAndDelete(commentId);
+
+         return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Comments was deleted"
+        )
+    )
+})
+
+export {
+    getVideoComments, 
+    addComment, 
+    updateComment,
+     deleteComment
+    }
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,26 +5,121 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    
-    // get video
-    // like could already exist
-})
+    const { videoId } = req.params;
+
+    // 1. Check if the like already exists for THIS user
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id // Match the specific user
+    });
+
+    if (existingLike) {
+        // 2. If it exists, delete the record (Unlike)
+        await Like.findByIdAndDelete(existingLike._id);
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "The Video was Unliked Successfully")
+        );
+    }
+
+    // 3. If it doesn't exist, create it (Like)
+    const newLike = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id // Use the field name from your schema
+    });
+
+    if (!newLike) {
+        throw new ApiError(500, "There was some error in liking the video");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, newLike, "The Video was Liked Successfully")
+    );
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+  const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id // Match the specific user
+    });
 
-})
+    if (existingLike) {
+        // 2. If it exists, delete the record (Unlike)
+        await Like.findByIdAndDelete(existingLike._id);
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "The comment was Unliked Successfully")
+        );
+    }
+
+    // 3. If it doesn't exist, create it (Like)
+    const newLike = await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id // Use the field name from your schema
+    });
+
+    if (!newLike) {
+        throw new ApiError(500, "There was some error in liking the comment");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, newLike, "The comment was Liked Successfully")
+    );
+});
+
+
+
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
-    //TODO: toggle like on tweet
+    const existingLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id // Match the specific user
+    });
+
+    if (existingLike) {
+        // 2. If it exists, delete the record (Unlike)
+        await Like.findByIdAndDelete(existingLike._id);
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "The tweet was Unliked Successfully")
+        );
+    }
+
+    // 3. If it doesn't exist, create it (Like)
+    const newLike = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id // Use the field name from your schema
+    });
+
+    if (!newLike) {
+        throw new ApiError(500, "There was some error in liking the tweet");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, newLike, "The tweet was Liked Successfully")
+    );
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+
+        const likedVideos = await Like.find({});
+        
+        if(!likedVideos) {
+            throw new ApiError(400,"Couldn't fetch the videos");
+        }
+
+        res.status(200).
+        json(
+            new ApiResponse(200,
+                likedVideos,
+                "The Liked Videos fetched Successfully"
+            )
+        )
 })
 
 export {
