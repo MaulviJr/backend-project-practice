fbd90031d58e746193a9b36ca1aaffd9b99a1e66
MaulviJr <abdulhadighouri123@gmail.com>
2026-01-06 16:14:14 +0500

    feat: add comment, like, and playlist routes; implement JWT verification for all routes

--- src/app.js
+++ src/app.js
@@ -20,11 +20,16 @@ import router from './routes/user.route.js';
 import videoRouter  from './routes/video.route.js';
 import tweetRouter  from './routes/tweet.route.js';
 import subscriptionRouter from "./routes/subscription.route.js";
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 app.use("/api/v1/users",router);
 app.use("/api/v1/videos",videoRouter);
 app.use("/api/v1/tweets",tweetRouter);
 app.use("/api/v1/subscription",subscriptionRouter);
-
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 export {app};
