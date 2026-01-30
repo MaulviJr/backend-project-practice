0952cc6d66587bbf5db9190a77bcfd959127b8b2
MaulviJr <abdulhadighouri123@gmail.com>
2026-01-30 18:07:29 +0500

    fix: correct endpoint for playlists route in app.js

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use("/api/v1/tweets",tweetRouter);
 app.use("/api/v1/subscription",subscriptionRouter);
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/healthcheck", healthcheckRouter)
 
