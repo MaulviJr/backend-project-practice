ced8a237d7860c5f0fdb1437f144a2cd5c6aa292
MaulviJr <abdulhadighouri123@gmail.com>
2026-02-10 16:37:41 +0500

    fix: remove console log from isPasswordCorrect method

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,7 +51,7 @@ userSchema.pre("save", async function () {
 })
 
 userSchema.methods.isPasswordCorrect= async function (password) {
-    console.log("I am checking password: ", password)
+    // console.log("I am checking password: ", password)
    return await bcrypt.compare(password, this.password);
 }
 userSchema.methods.generateAccessToken = function() {
