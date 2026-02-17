a688af1879ca588d71c5edbebc122755948ae028
MaulviJr <abdulhadighouri123@gmail.com>
2026-02-17 17:52:29 +0500

    refactor: improve code formatting and consistency in tweet.controller.js

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,40 +1,40 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Twitter as Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Twitter as Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     // take tweet content from req.body
     // implicitly update owner by extracting from req.user;
 
-    const {content}= req.body;
+    const { content } = req.body;
 
-    if(!content) {
+    if (!content) {
         throw new ApiError(400, "Can't create empty tweet");
     }
     console.log("this is user: ", req.user);
-    const tweet = await Tweet.create( {
+    const tweet = await Tweet.create({
         content: content,
         owner: req.user?._id,
     })
 
-    if(!tweet) {
+    if (!tweet) {
         throw new ApiError(404, "the tweet can't be created");
     }
 
     return res.status(200)
-    .json(
-        new ApiResponse(200,
-            tweet,
-            "The tweet was created successfully"
+        .json(
+            new ApiResponse(200,
+                tweet,
+                "The tweet was created successfully"
+            )
         )
-    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    const {userId} = req.params
+    const { userId } = req.params
 
     if (!mongoose.isValidObjectId(userId)) {
         throw new ApiError(400, "Invalid user ID format");
@@ -43,21 +43,49 @@ const getUserTweets = asyncHandler(async (req, res) => {
     // grab user id,
     // then traverse through the tweets and pick up the ones whose user id matches the owner.
 
-    const userTweets = await Tweet.find(
-        {owner:userId}
-    )
+    // const userTweets = await Tweet.find(
+    //     {owner:userId}
+    // )
 
-    if(!userTweets) {
-        throw new ApiError(400,"couldn't fetch user tweets")
+  const userTweets = await Tweet.aggregate([
+  {
+    $match: {
+      owner: new mongoose.Types.ObjectId(userId),
+    },
+  },
+  {
+    $lookup: {
+      from: "users",
+      localField: "owner",
+      foreignField: "_id",
+      as: "owner",
+      pipeline: [
+        {
+          $project: {
+            username: 1,
+            fullName: 1,
+            avatar: 1,
+          },
+        },
+      ],
+    },
+  },
+  {
+    $unwind: "$owner",
+  },
+]);
+
+    if (!userTweets) {
+        throw new ApiError(400, "couldn't fetch user tweets")
     }
 
     return res.status(200)
-    .json(
-        new ApiResponse(200,
-            userTweets,
-            "The tweets of the User fetched successfully!"
+        .json(
+            new ApiResponse(200,
+                userTweets,
+                "The tweets of the User fetched successfully!"
+            )
         )
-    )
     // TODO: get user tweets
 })
 
@@ -67,24 +95,24 @@ const updateTweet = asyncHandler(async (req, res) => {
     // take the content only
     // update
 
-    const {tweetId} = req.params
+    const { tweetId } = req.params
 
     if (!mongoose.isValidObjectId(tweetId)) {
         throw new ApiError(400, "Invalid Tweet ID format");
     }
-    const {content} = req.body
+    const { content } = req.body
     const getTweet = await Tweet.findById(tweetId);
 
-    if(!getTweet) {
+    if (!getTweet) {
         throw new ApiError(404, "There was some error fetching the tweet")
     }
-        // check if current user is the owner of the tweet
-      
-    if(!(getTweet.owner.equals(req.user.id))) {
-        throw new ApiError(400,"The User is not the owner of the tweet")
+    // check if current user is the owner of the tweet
+
+    if (!(getTweet.owner.equals(req.user.id))) {
+        throw new ApiError(400, "The User is not the owner of the tweet")
     };
-    
-    if(!content) {
+
+    if (!content) {
         throw new ApiError(400, "The tweet content is empty")
     }
     getTweet.content = content;
@@ -92,46 +120,46 @@ const updateTweet = asyncHandler(async (req, res) => {
     await getTweet.save()
 
     return res.status(200)
-    .json (
-        new ApiResponse(200,
-            getTweet,
-            "The tweet was updated successfully"
+        .json(
+            new ApiResponse(200,
+                getTweet,
+                "The tweet was updated successfully"
+            )
         )
-    )
 
 
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
-    const {tweetId} = req.params
+    const { tweetId } = req.params
 
     if (!mongoose.isValidObjectId(tweetId)) {
         throw new ApiError(400, "Invalid Tweet ID format");
     }
 
-     const getTweet = await Tweet.findById(tweetId);
+    const getTweet = await Tweet.findById(tweetId);
 
-       if(!getTweet) {
+    if (!getTweet) {
         throw new ApiError(404, "There was some error fetching the tweet")
     }
 
     console.log("req.user : ", req.user);
     console.log("get tweet oiwner", getTweet);
-    
-    if(!(getTweet.owner.equals(req.user.id))) {
-        throw new ApiError(400,"The User is not the owner of the tweet")
+
+    if (!(getTweet.owner.equals(req.user.id))) {
+        throw new ApiError(400, "The User is not the owner of the tweet")
     };
 
-       const deletedTweet=await Tweet.findByIdAndDelete(tweetId);
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
 
     return res.status(200)
-    .json(
-        new ApiResponse(200,
-            deletedTweet,
-            "Tweet Deleted Successfully!"
+        .json(
+            new ApiResponse(200,
+                deletedTweet,
+                "Tweet Deleted Successfully!"
+            )
         )
-    )
 })
 
 export {
