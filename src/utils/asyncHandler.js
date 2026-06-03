8712b39b3c4077b775d51eaf72cb41fffa3a9245
MaulviJr <abdulhadighouri123@gmail.com>
2026-06-03 19:06:49 +0500

    refactor: clean up commented-out code in asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,24 +1,26 @@
 
 
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+//     }
+// }
+
+
+// export { asyncHandler }
+
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch(error) {
+        // This explicitly forces Express to send JSON back to your frontend
+        res.status(error.statusCode || 500).json({
+            success: false,
+            message: error.message || "Internal Server Error",
+            errors: error.errors || []
+        });
     }
 }
 
-
-export { asyncHandler }
-
-
-// const asyncHandler=(fn)=> async (req,res,next)=>{
-//     try {
-//         await fn(req,res,next);
-
-//     }catch(error) {
-//             res.status(error.code || 500).json({
-//                 success: false,
-//                 message: error.message
-//             })
-//     }
-// }
-// export {asyncHandler}
+export { asyncHandler };
